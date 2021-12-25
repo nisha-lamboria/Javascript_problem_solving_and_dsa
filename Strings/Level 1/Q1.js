ef740d326712003dd5ccdb19f6b70c501e84ca88
nisha4545 <88261286+nisha4545@users.noreply.github.com>
2021-12-25 19:02:14 +0530

    string questions done-25/12/2021

---
+++ Strings/Level 1/Q1.js
@@ -0,0 +1,94 @@
+//A
+string1="batgirl"
+string1=string1.toUpperCase()
+console.log(string1)
+
+//B
+stringB="wolverine"
+stringB1="deadpool"
+stringNew=stringB1.concat(stringB,string1)
+console.log(stringNew)
+
+//C
+stringC="NishaLamboria"
+console.log(stringC.length)
+
+//D
+stringD="IdentifyBugs"
+function removeVowel(getString){
+    vowels=['a','e','i','o','u','A','E','I','O','U']
+    res=""
+    for(let i=0;i<getString.length;i++){
+        if(!vowels.includes(getString[i])){
+            res+=getString[i]
+        }
+    }
+    console.log(res)
+}
+removeVowel(stringD)
+
+//E
+stringsE=["short","middle","longest"]
+function maxmin(stringsE){
+    max=""
+    min="100000"
+    for(let i=0;i<stringsE.length;i++){
+        if(stringsE[i].length>max.length){
+            max=stringsE[i]
+        }
+        if(stringsE[i].length<min.length){
+            min=stringsE[i]
+        }
+    }
+    console.log(max,min)
+}
+maxmin(stringsE)
+
+//F
+let stringsF="countConsonantsandVowels"
+function countConsonantsAndVowels(){
+    vowels=['a','e','i','o','u','A','E','I','O','U']
+    countVow=0
+    countCons=0
+    for(let i=0;i<stringsF.length;i++){
+        if(vowels.includes(stringsF[i])){
+            countVow+=1
+        }else{
+            countCons+=1
+        }    
+    }
+    console.log(`vowelsCount:${countVow} ConsonantsCount:${countCons}`)
+}
+countConsonantsAndVowels()
+
+//G
+let stringsG="logan@45"
+function checkAlphaNumeric(){
+    for(let i=0;i<stringsG.length;i++){
+        let code=stringsG.charCodeAt(i)
+        if(!(code > 47 && code < 58) &&
+        !(code > 64 && code < 91) &&
+        !(code > 96 && code < 123)){
+            return false;
+        }
+    }
+    // console.log('alphanumeric')
+    return true;
+}
+let alphanumeResult=checkAlphaNumeric()
+console.log(alphanumeResult)
+
+//H
+let stringsH="stringLength"
+function checkLength(str){
+    let lengthCount=0
+    for(s in str){
+        lengthCount+=1
+    }
+    return lengthCount 
+}
+let stringLengthResult=checkLength(stringsH)
+console.log(stringLengthResult)
+
+
+
---
+++ Strings/readme.md
@@ -0,0 +1,10 @@
+Level 1
+
+Q1 >  a)Write a program that converts the string into uppercase.
+b)Write a program that reads two strings and append first string to the second.
+c)Program that reads string and count number of characters present in the string.
+d)Write a program to delete all vowels from a string. Assume string is not more than 80 characters long.
+e)A program that reads three strings and prints the longest and smallest one.
+f)A program that counts number of vowels and consonants in a String.
+g)Write a program to check whether the string is alphanumeric or not , eg:logan@45 contains digit 45.
+h)Write a program which receives a string str that calculates the length of a string and return true if the length is greater than 7 without using strlen()
